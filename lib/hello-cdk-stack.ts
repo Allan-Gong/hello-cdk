003414e66047bf6266cd58cdaf64172a7914ff36
Allan Gong <hanyun.gong@gmail.com>
2024-06-03 20:27:40 -0700

    anomaly detection alarm based on math expression

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,45 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const metricStat =
+      cdk.aws_lambda.Function.metricAllErrors().toMetricConfig().metricStat;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cfnAlarm = new cdk.aws_cloudwatch.CfnAlarm(this, "anomalyDetectionMathAlarm", {
+      alarmName: "anomaly-detection-math-alarm",
+      metrics: [
+        {
+          id: "ad1",
+          expression: "ANOMALY_DETECTION_BAND(me1, 2)",
+          returnData: true,
+        },
+        {
+          id: "me1",
+          expression: "IF(m1 > 10, m1, 0)",
+          returnData: true,
+        },
+        {
+          id: "m1",
+          metricStat: {
+            metric: {
+              metricName: metricStat?.metricName,
+              namespace: metricStat?.namespace,
+              dimensions: metricStat?.dimensions,
+            },
+            period: cdk.Duration.minutes(5).toSeconds(),
+            stat: cdk.aws_cloudwatch.Stats.SUM,
+          },
+          returnData: false,
+        },
+      ],
+      datapointsToAlarm: 5,
+      evaluationPeriods: 5,
+      comparisonOperator:
+        cdk.aws_cloudwatch.ComparisonOperator.GREATER_THAN_UPPER_THRESHOLD,
+      thresholdMetricId: "ad1",
+    });
   }
 }
